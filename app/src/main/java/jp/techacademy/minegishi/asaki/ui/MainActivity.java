ce4a0ec046ed3fd761d32dda8a7328c628e59c19
asaki <asaki.minegishi@music.yamaha.com>
2017-05-12 12:33:14 +0900

    カリキュラム3.1 イベントリスナー

--- app/src/main/java/jp/techacademy/minegishi/asaki/ui/MainActivity.java
+++ app/src/main/java/jp/techacademy/minegishi/asaki/ui/MainActivity.java
@@ -1,10 +1,12 @@
 package jp.techacademy.minegishi.asaki.ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Button;  //Buttonクラスを使う時
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button1 = (Button) findViewById(R.id.button1);
-        button1.setText("ボタン");
+        button1.setOnClickListener(this);
+        // setOnClickListenerメソッドの引数にはonClickメソッドを実装した
+        // クリックリスナークラスを渡す必要がある。
+        // 今回はMainActivityクラス自身(this)を設定。
+    }
+
+    @Override
+    public void onClick(View v) {  // onClickメソッドを実装
+        Log.d("UI_PARTS", "ボタンをタップしました");
     }
 }
