2c65efc9af76ed2491be6321cc6944dbf800128b
asaki <asaki.minegishi@music.yamaha.com>
2017-05-12 13:07:27 +0900

    Lesson4 カリキュラム3.3

--- app/src/main/java/jp/techacademy/minegishi/asaki/ui/MainActivity.java
+++ app/src/main/java/jp/techacademy/minegishi/asaki/ui/MainActivity.java
@@ -6,23 +6,31 @@ package jp.techacademy.minegishi.asaki.ui;
         import android.view.View;
         import android.widget.Button;
         import android.widget.TextView;
+        import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // メンバ変数
+    TextView mTextView;
+    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {  //new View.OnClickListener() {…}無名クラス
-            @Override                                            // OnClickListenerクラスを実装したクラスを指定する以外に直接記述することもできる。
-            public void onClick(View v) {
-                Log.d("UI_PARTS", "ボタンをタップしました");
-            }
-        });
+        Button button1 = (Button) findViewById(R.id.button1);  // (Button)というのはButtonクラスにキャストするという意味
+        button1.setOnClickListener(this);                      // findViewByIdメソッドで取得したViewのインスタンスをButtonクラスに変換して、
+                                                               // button1という変数に入れている
+                                                               // ButtonクラスはViewクラスを継承しているので、キャストすることができる
 
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("テキスト");
+        mTextView = (TextView) findViewById(R.id.textView);  // インスタンスを代入
+        mEditText = (EditText) findViewById(R.id.editText);  // インスタンスを代入
     }
+
+    @Override
+    public void onClick(View v) {
+        mTextView.setText(mEditText.getText().toString());  // EditTextに入力された文字列をStringクラスのインスタンスとして取得
+    }   // getTextメソッドではStringクラスではなく、Editableインタフェースを実装した何らかのクラスのインスタンス。
+        // このままではTextViewに設定することができないのでtoStringメソッドでStringクラスに変換
 }
